fbfa51ed24c497c0e7bc49f21caebf27681aa248
Danaferh95 <Danaferh95@gmail.com>
2024-07-29 18:22:03 +0200

    Se empieza a vincular los videos, se cargara a git para empezar a probar la privacidad

---
+++ js/funciones.js
@@ -0,0 +1,68 @@
+
+
+/*Función para mover la posición del scroll con más precisión dentro de la web*/
+
+const links = document.querySelectorAll("a");
+const offset = 100; //Pixeles a mover
+
+
+links.forEach(link => {
+    link.addEventListener("click", function(event) {
+        event.preventDefault();
+        const targetId = this.getAttribute("href").substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop - offset,
+                behavior: "smooth"
+            });
+        }
+    });
+});
+
+
+
+/*--------------------------------------------------------
+
+	MENU DESPLEGABLE
+		
+	En esta sección hacemos el toggle del menu principal, donde se vuelve 
+	un menu hamburguesa cuando el dispositivo es pequeño
+
+--------------------------------------------------------*/
+
+const navegacion = document.querySelector("nav"); 
+const btnNav = document.querySelectorAll(".abrir , .cerrar"); 
+
+
+btnNav.forEach((obj)=>{
+
+	obj.addEventListener("click",()=>{
+		navegacion.classList.toggle("desplegado");
+	});
+
+
+}); 
+
+
+/*
+
+Funcion para que el video solo se de play cuando se hace hover
+
+*/
+
+const videoContainers = document.querySelectorAll(".video-container");
+
+videoContainers.forEach(container => {
+    const iframe = container.querySelector("iframe");
+    const player = new Vimeo.Player(iframe);
+
+    container.addEventListener("mouseenter", function() {
+        player.play();
+    });
+
+    container.addEventListener("mouseleave", function() {
+        player.pause();
+    });
+});
