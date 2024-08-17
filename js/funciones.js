bb2c64f441ada4c739205e0ba787e537e63f6f78
Danaferh95 <Danaferh95@gmail.com>
2024-08-17 15:06:04 +0200

    Deleted unnecesary files

--- js/funciones.js
+++ js/funciones.js
@@ -1,24 +1,3 @@
-/*--------------------------------------------------------
- Function to precisely move the scroll position within the web page 
- --------------------------------------------------------*/
-
-const links = document.querySelectorAll("nav a");
-const offset = 0; // Pixels to move
-
-links.forEach(link => {
-    link.addEventListener("click", function(event) {
-        event.preventDefault();
-        const targetId = this.getAttribute("href").substring(1);
-        const targetElement = document.getElementById(targetId);
-
-        if (targetElement) {
-            window.scrollTo({
-                top: targetElement.offsetTop - offset,
-                behavior: "smooth"
-            });
-        }
-    });
-});
 
 /*--------------------------------------------------------
     MENU TOGGLE
@@ -34,73 +13,6 @@ btnNav.forEach((obj) => {
         navegacion.classList.toggle("desplegado");
     });
 }); 
-
-
-/*--------------------------------------------------------
-Function to play the videos
- --------------------------------------------------------*/
-
-const videos = document.querySelectorAll("video");
-let currentPlayingVideo = null;
-
-videos.forEach(video => {
-
-    function playVideo() {
-        // Pause the currently playing video if it's different from the one being played
-        if (currentPlayingVideo && currentPlayingVideo !== video) {
-            currentPlayingVideo.pause();
-        }
-
-        // Play the new video
-        video.play();
-        // Update the current playing video
-        currentPlayingVideo = video;
-    }
-
-    function pauseVideo() {
-        // Only pause if this video is the currently playing one
-        if (currentPlayingVideo === video) {
-            video.pause();
-         };
-            currentPlayingVideo = null; 
-    }
-  
-
-    video.addEventListener("mouseenter", playVideo);
-    video.addEventListener("mouseleave", pauseVideo);
-
-    video.addEventListener("touchstart", function(event) {
-        playVideo();
-    });
-
-     // Prevent context menu from appearing on long press
-     video.addEventListener("contextmenu", function(event) {
-        event.preventDefault();
-    });
-    
-});
-
-/*--------------------------------------------------------
-Function to switch the href as needed in the UX/UI section
- --------------------------------------------------------*/
-
- const hrefs = document.querySelectorAll(".dynamicLink");
-
-    function isMobile() {
-        return /Mobi|Android/i.test(navigator.userAgent);
-    }
-
-    hrefs.forEach(link => {
-        const desktopLink = link.getAttribute("data-desktop");
-        const mobileLink = link.getAttribute("data-mobile");
-
-        if (isMobile()) {
-            link.href = mobileLink;
-        } else {
-            link.href = desktopLink;
-        }
-    });
-
 
 
 /*-----------------------------------
